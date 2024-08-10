804830a32133acf93719a1ec835cb359752c5e8f
Damilare Bakare <bakaredmlr@gmail.com>
2024-08-10 08:36:06 +0100

    creates activity page: include custom button, connection card

---
+++ task/components/CustomButton.tsx
@@ -0,0 +1,28 @@
+import { Colors } from '@/constants/Colors';
+import { Button, Text } from '@gluestack-ui/themed';
+import React from 'react'
+
+const CustomButton = ({
+    onPress,
+    title,
+}: {
+    onPress: () => void;
+    title: string;
+}) => {
+  return (
+    <Button
+      rounded="$xl"
+      borderWidth={1.5}
+      borderColor={Colors.tint}
+      variant="outline"
+      onPress={() => {}}
+      marginTop="$4"
+    >
+      <Text fontSize="$xs" fontWeight="$semibold" color={Colors.tint}>
+        {title}
+      </Text>
+    </Button>
+  );
+}
+
+export default CustomButton
--- task/constants/Colors.ts
+++ task/constants/Colors.ts
@@ -5,10 +5,11 @@
 const primaryColor = "#CE1694";
 
 export const Colors = {
-    text: "#11181C",
-    background: "#fff",
-    tint: primaryColor,
-    icon: "#687076",
-    tabIconDefault: "#D4D6DD",
-    tabIconSelected: primaryColor,
+  text: "#1F2024",
+  background: "#fff",
+  tint: primaryColor,
+  icon: "#687076",
+  tabIconDefault: "#D4D6DD",
+  tabIconSelected: primaryColor,
+  cardBackground: "#F8F9FE",
 };
