8319bbbc2278effccd6e3070382aef6bad3f3d25
Damilare Bakare <bakaredmlr@gmail.com>
2024-08-11 04:49:07 +0100

    adds user type

--- task/types/UserType.ts
+++ task/types/UserType.ts
@@ -1,3 +1,27 @@
 interface User {
-    id: number
+  id: number;
+  first_name: string;
+  last_name: string;
+  gender: string;
+  bio: string;
+  score: number;
+  location: LocationType;
+  dob: string;
+  photos: PhotoType[];
+  interests: InterestsType[];
 }
+
+type LocationType = {
+  city: string;
+  country: string;
+};
+
+type PhotoType = {
+  id: number;
+  path: string;
+};
+
+type InterestsType = {
+  id: number;
+  name: string;
+};
